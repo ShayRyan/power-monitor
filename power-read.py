de7782c02f041846d79ecd7bcd29f8df8899c2b2
Shay Ryan <shay.ryan@outlook.com>
2023-01-17 23:53:52 +0000

    add main. add log to csv

--- power-read.py
+++ power-read.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from datetime import datetime
 import serial
+import csv
 import xml.etree.ElementTree as et
 
 ser = serial.Serial(
@@ -11,6 +12,9 @@ ser = serial.Serial(
     bytesize=serial.EIGHTBITS,
     timeout=3
 )
+
+log_file_name = "log.csv"
+header = ['timestamp', 'local_time', 'sensor_time', 'temp', 'power', 'energy']
 
 def decode_xml(xml_string):
     """Decode an XML string to the full extent from root to leaves."""
@@ -40,39 +44,56 @@ def calc_energy(pwr, sec):
 
     return kilo_watt_hours
 
-last_ts = 0
-while True:
-    # Time of reading
-    now = datetime.now()
-    this_ts = datetime.timestamp(now)
-    local_time = now.strftime("%Y-%m-%d %H:%M:%S")
+def main():
 
-    line = ser.readline()
-    line = line.strip(b'\r\n')
-    line = line.decode('utf-8')
-    if line:
-        decode_xml(line)
-        xml_dict = decode_xml(line)
-        #print(xml_dict)
+    last_ts = 0
 
-        if 'watts' in xml_dict:
-            sensor_time = xml_dict['time'][0]
-            power = int(xml_dict['watts'][0])
-            tempr = float(xml_dict['tmpr'][0])
+    try:
+        with open(log_file_name, mode='w', newline='') as log_file:
+            log_writer = csv.writer(log_file)
+            log_writer.writerow(header)
 
-            if last_ts == 0:
-                energy = 0
-            else:
-                energy = calc_energy(power, (this_ts - last_ts))
+            while True:
+                # Time of reading
+                now = datetime.now()
+                this_ts = datetime.timestamp(now)
+                local_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
-            print(f"{local_time} -> Sensor Time= {sensor_time} "
-                  f"Temp= {tempr} C Power= {power} W Energy= {energy} kWh")
+                line = ser.readline()
+                line = line.strip(b'\r\n')
+                line = line.decode('utf-8')
+                if line:
+                    decode_xml(line)
+                    xml_dict = decode_xml(line)
+                    #print(xml_dict)
 
-            last_ts = this_ts
+                    if 'watts' in xml_dict:
+                        sensor_time = xml_dict['time'][0]
+                        power = int(xml_dict['watts'][0])
+                        tempr = float(xml_dict['tmpr'][0])
+
+                        if last_ts == 0:
+                            energy = 0
+                        else:
+                            energy = calc_energy(power, (this_ts - last_ts))
+
+                        row = [this_ts, local_time, sensor_time, tempr, power, energy]
+                        log_writer.writerow(row)
+
+                        #print(f"{local_time} -> Sensor Time= {sensor_time} "
+                        #      f"Temp= {tempr} C Power= {power} W Energy= {energy} kWh")
+
+                        last_ts = this_ts
+    except KeyboardInterrupt:
+        print("Keyboard interrupt detected, closing log file and exiting...")
+    finally:
+        log_file.close()
+
+if __name__ == "__main__":
+    main()
 
 # add main
 
-# add timestamp for time of receipt
-# print tor and message
+# add time of receipt time delta and sensor timestamp time delta
 # write to CSV
 # write to SQLite DB
